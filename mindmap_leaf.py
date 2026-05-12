d7a806450e38da98f77be54b7c4d50d7ef964c36
danielv1-lab <danielv1@aldergrovecharter.org>
2026-05-11 22:01:57 -0700

    done

---
+++ mindmap_leaf.py
@@ -0,0 +1,21 @@
+class MindMapLeaf:
+    def __init__(self, name, shape):
+        self.name = name
+        self.shape = shape
+
+    def get_shape_representation(self):
+        shapes = {
+            "circle": "(({}))",
+            "ovel": ("({})"),
+            "square": "[{}]",
+            "cloud": "){}(",
+            "hexagon": "{{{{{}}}}}",
+            "bang": ")){}(("
+        }
+        return shapes.get(self.shape, '{}')
+
+    def display(self, indent=0):
+        print(' ' * indent + str(self))
+
+    def __str__(self):
+        return self.get_shape_representation().format(self.name)
